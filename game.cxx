f6acff5741152dff8b98d2cd7c7d49c210ef794c
Darek <dar.karasiewicz@gmail.com>
2022-04-08 20:27:58 +0200

    add timeout to input user

--- game.cxx
+++ game.cxx
@@ -1,13 +1,31 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string>
-
+#include <string.h>
+#include <sys/select.h>
 
 auto main() -> int
 {
+
+    char buff[100];
+    auto sz = ssize_t{};
+    memset(buff,0,100);
+
     while(true){
-        char buff[100];
-        auto const sz= read(0,buff,100);
+        fd_set readfds;
+        FD_ZERO(&readfds);
+        FD_SET(0,&readfds);
+        auto const nfds= 0 + 1;
+
+        timeval timeout{5,0};
+
+        if(select(nfds,&readfds,nullptr,nullptr,&timeout) == -1){
+            break;
+        }
+        if(FD_ISSET(0,&readfds)){
+            sz= read(0,buff,100);
+        }
+
         write(1,buff,sz);
     }
     return 0;
