987581862e91177360819c4e63173bda5fead37a
Darek <dar.karasiewicz@gmail.com>
2022-04-29 20:41:21 +0200

    better map and improved MakeFile

--- game.cxx
+++ game.cxx
@@ -4,20 +4,23 @@
 #include <string.h>
 #include <sys/select.h>
 
+#include "map.h"
+
 auto main() -> int
 {
+    create_map(25,15);
 
     char buff[100];
     auto sz = ssize_t{};
     memset(buff,0,100);
 
-    while(true){
+    while(false){
         fd_set readfds;
         FD_ZERO(&readfds);
         FD_SET(0,&readfds);
         auto const nfds= 0 + 1;
 
-        timeval timeout{5,0};
+        timeval timeout{1,0};
 
         if(select(nfds,&readfds,nullptr,nullptr,&timeout) == -1){
             break;
