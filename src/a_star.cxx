54beb24a05a56c5175fad68e6d31e10cd7fffc86
Darek Karsiewicz <dar.karasiewicz@gmail.com>
2022-06-30 21:16:59 +0200

    A* implementation

--- src/a_star.cxx
+++ src/a_star.cxx
@@ -1,11 +1,17 @@
-#include <fstream>
-#include <iostream>
-#include <string>
-#include <vector>
 #include <cmath>
-#include <queue>
-#include <utility>
+#include <fstream>
 #include <functional>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <utility>
+#include <vector>
+#include <stack>
+#include <optional>
+#include <thread>
+#include <chrono>
 
 enum class Field {
     PLAYER = '@',
@@ -25,14 +31,77 @@ auto get_field(board_type& board, size_t x, size_t y) -> Field&
 {
     return board[y][x];
 }
-auto print_board(board_type const& board) ->void
+auto get_field(board_type const& board, size_t x, size_t y) -> Field
 {
-    for(auto const& row : board ){
-        for(auto const f : row){
-                std::cout << to_string(f);
+    return board[y][x];
+}
+auto print_board(board_type const& board) -> void
+{
+    for (auto const& row : board) {
+        for (auto const f : row) {
+            std::cout << to_string(f);
         }
         std::cout << "|\n";
     }
+}
+auto is_valid_field(board_type const& board ,std::pair<size_t,size_t> p )->bool
+{
+    auto const [x,y] = p;
+    return (y < board.size() and x < board.front().size());
+}
+auto is_valid_move(board_type const& board ,std::pair<size_t,size_t> p )->bool
+{
+    if (!is_valid_field(board, p)){
+        return false;
+    }
+    auto const [x,y] = p;
+    auto const f = get_field(board, x, y);
+    if(f == Field::WALL){
+        return false;
+    }
+    return true;
+
+}
+auto neighbors(board_type const& board, size_t x, size_t y)
+    -> std::vector<std::pair<size_t, size_t>>
+{
+    auto close_friends = std::vector<std::pair<size_t, size_t>>{};
+    auto up            = std::pair<size_t, size_t>{x, y + 1};
+    auto down          = std::pair<size_t, size_t>{x, y - 1};
+    auto left          = std::pair<size_t, size_t>{x - 1, y};
+    auto right         = std::pair<size_t, size_t>{x + 1, y};
+    for(auto each : {up, down, left, right}){
+        if(is_valid_move(board, each)){
+            close_friends.push_back(each);
+        }
+    }
+    return close_friends;
+}
+
+auto set_score(std::map<std::pair<size_t, size_t>, float>& gscore, std::pair<size_t, size_t> pair, float value) ->void
+{
+        gscore.insert({pair,value});
+}
+
+auto get_score(std::map<std::pair<size_t, size_t>, float>& gscore, std::pair<size_t, size_t> pair) -> float
+{
+        if(gscore.contains(pair))
+        {
+            return gscore.at(pair);
+        }
+        return INFINITY;
+}
+using pos_type = std::pair<size_t, size_t>;
+auto reconstruct_path(std::map<pos_type,pos_type> came_from, pos_type goal) ->std::stack<pos_type>
+{
+    auto total_path = std::stack<pos_type>{};
+    /* goal = came_from[goal]; */
+    while (came_from.contains(goal)){
+        goal = came_from[goal];
+        total_path.push(goal);
+    }
+    total_path.pop();
+    return total_path;
 }
 
 auto main() -> int
@@ -66,18 +135,17 @@ auto main() -> int
         }
     }
 
-    using pos_type = std::pair<size_t,size_t>;
     pos_type player, goal;
     {
         for (size_t y = 0; y < board.size(); ++y) {
             for (size_t x = 0; x < board.front().size(); ++x) {
                 switch (board[y][x]) {
                 case Field::PLAYER:
-                    player.first = x;
+                    player.first  = x;
                     player.second = y;
                     break;
                 case Field::GOAL:
-                    goal.first = x;
+                    goal.first  = x;
                     goal.second = y;
                     break;
                 default:
@@ -85,27 +153,95 @@ auto main() -> int
                 }
             }
         }
-        std::cout << to_string(get_field(board, player.first, player.second)) << "\n";
-        std::cout << to_string(get_field(board, goal.first, goal.second)) << "\n";
+        std::cout << to_string(get_field(board, player.first, player.second))
+                  << "\n";
+        std::cout << to_string(get_field(board, goal.first, goal.second))
+                  << "\n";
     }
     print_board(board);
 
-    auto const h = [goal](size_t x, size_t y) -> double
-    {
+    auto const h = [goal](size_t x, size_t y) -> double {
         auto const a = std::llabs(goal.first - x);
         auto const b = std::llabs(goal.second - y);
-        return std::sqrt((a*a) + (b*b));
+        return std::sqrt((a * a) + (b * b));
     };
-    static_cast<void>(h);
 
-    auto q = std::priority_queue<
-        std::pair<double, pos_type>,
-        std::vector<std::pair<double, pos_type>>,
-        std::greater<std::pair<double, pos_type>>>{};
-    q.push({1.0,player});
-    q.push({0.3,player});
+    auto open_queue =
+        std::priority_queue<std::pair<double, pos_type>,
+                            std::vector<std::pair<double, pos_type>>,
+                            std::greater<std::pair<double, pos_type>>>{};
+    auto open_set           = std::set<pos_type>{};
+    auto const push_to_open = [&open_queue, &open_set](
+                                  double prority, pos_type position) -> void {
+        if (open_set.contains(position)) {
+            return;
+        }
+        open_queue.push({prority, position});
+        open_set.insert(position);
+    };
+    auto const pop_from_open = [&open_queue, &open_set]() -> pos_type {
+        auto const node = open_queue.top().second;
+        open_queue.pop();
+        open_set.erase(node);
+        return node;
+    };
+    auto l = neighbors(board, 0, 0);
+    std::cout << l.size() << "\n";
 
-    std::cout << q.top().first <<"\n";
+    push_to_open(0, player);
+
+    auto came_from = std::map<pos_type,pos_type>{};
+
+    std::map<pos_type, float> gscore;
+    set_score(gscore, player, 0);
+
+    std::map<pos_type, float> fscore;
+    set_score(fscore, player, h(player.first, player.second));
+
+    auto path = std::optional<std::stack<pos_type>>{};
+    while (!open_set.empty()) {
+        auto const current = pop_from_open();
+        auto &f = get_field(board, current.first, current.second);
+        if(f != Field::PLAYER and f != Field::GOAL){
+            f = Field::EMPTY;
+        }
+        if (current == goal) {
+            path = reconstruct_path(came_from, goal);
+            break;
+        }
+
+        for(auto neighbor : neighbors(board, current.first, current.second))
+        {
+            auto tentative_gscore = get_score(gscore, current) + 1;
+            if (tentative_gscore < get_score(gscore, neighbor)){
+                came_from[neighbor] = current;
+                gscore[neighbor] = tentative_gscore;
+                fscore[neighbor] = tentative_gscore + h(neighbor.first, neighbor.second);
+                push_to_open(tentative_gscore, neighbor);
+                auto &f = get_field(board, neighbor.first, neighbor.second);
+                if (f != Field::GOAL){
+                    f= Field::OPEN;
+                }
+            }
+        }
+
+        print_board(board);
+        std::cout <<"\n";
+        std::this_thread::sleep_for(std::chrono::milliseconds{167});
+    }
+
+    if (!path.has_value()){
+        return 1;
+    }
+    while (!path->empty())
+    {
+        auto step = path->top();
+        path->pop();
+
+       get_field(board, step.first, step.second) = Field::ROAD;
+
+    }
+    print_board(board);
 
     return 0;
 }
