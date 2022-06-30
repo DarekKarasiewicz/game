05c6eda5b7f7a4551f04d86c53fe6844d6263385
Darek Karsiewicz <dar.karasiewicz@gmail.com>
2022-06-30 19:56:12 +0200

    Detect player and goal

--- src/a_star.cxx
+++ src/a_star.cxx
@@ -8,11 +8,22 @@ enum class Field {
     GOAL   = 'x',
     WALL   = '#',
     EMPTY  = ' ',
+    ROAD   = '*',
 };
+auto to_string(Field field) -> std::string
+{
+    return std::string(1, static_cast<char>(field));
+}
+
+using board_type = std::vector<std::vector<Field>>;
+auto get_field(board_type& board, size_t x, size_t y) -> Field&
+{
+    return board[y][x];
+}
 
 auto main() -> int
 {
-    auto board = std::vector<std::vector<Field>>{};
+    auto board = board_type{};
     {
         auto in   = std::ifstream{"plansza.txt"};
         auto line = std::string{};
@@ -20,8 +31,6 @@ auto main() -> int
         auto const row_size = std::stoull(line);
         while (std::getline(in, line)) {
             line.resize(row_size, ' ');
-            std::cout << line << "|\n";
-
             board.push_back({});
 
             for (auto const each : line) {
@@ -42,26 +51,29 @@ auto main() -> int
             }
         }
     }
+
     struct {
         size_t x, y;
     } player, goal;
-    for (size_t y = 0; y < board.size(); ++y) {
-        for (size_t x = 0; x < board.front().size(); ++x) {
-            switch (board[y][x]) {
-            case Field::PLAYER:
-                player.x = x;
-                player.y = y;
-                break;
-            case Field::GOAL:
-                goal.x = x;
-                goal.y = y;
-                break;
-            default:
-                break;
+    {
+        for (size_t y = 0; y < board.size(); ++y) {
+            for (size_t x = 0; x < board.front().size(); ++x) {
+                switch (board[y][x]) {
+                case Field::PLAYER:
+                    player.x = x;
+                    player.y = y;
+                    break;
+                case Field::GOAL:
+                    goal.x = x;
+                    goal.y = y;
+                    break;
+                default:
+                    break;
+                }
             }
         }
+        std::cout << to_string(get_field(board, player.x, player.y)) << "\n";
+        std::cout << to_string(get_field(board, goal.x, goal.y)) << "\n";
     }
-    std::cout << player.x << " | " << player.y << "\n";
-    std::cout << goal.x << " | " << goal.y << "\n";
     return 0;
 }
